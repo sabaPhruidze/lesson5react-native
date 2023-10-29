f671834fa85b69a595a4b2fa6e4e11fc18e0036c
Saba Phruidze <sabaphruidze01@gmail.com>
2023-10-29 12:42:15 +0400

    we might not have a property key but a property id or else name, and in that case we have to use keyExtractor , where you are pointing what has to become a key

--- App.js
+++ App.js
@@ -2,29 +2,25 @@ import { StyleSheet, Text, View, FlatList } from "react-native";
 import React, { useState } from "react";
 export default function App() {
   const [people, setPeople] = useState([
-    { name: "shaun", key: 1 },
-    { name: "yoshi", key: 2 },
-    { name: "mario", key: 3 },
-    { name: "luigi", key: 4 },
-    { name: "peach", key: 5 },
-    { name: "toad", key: 6 },
-    { name: "bowser", key: 7 },
-    { name: "aaron", key: 8 },
-    { name: "specter", key: 9 },
-    { name: "lit", key: 10 },
+    { name: "shaun", id: 1 },
+    { name: "yoshi", id: 2 },
+    { name: "mario", id: 3 },
+    { name: "luigi", id: 4 },
+    { name: "peach", id: 5 },
+    { name: "toad", id: 6 },
+    { name: "bowser", id: 7 },
+    { name: "aaron", id: 8 },
+    { name: "specter", id: 9 },
+    { name: "lit", id: 10 },
   ]);
 
   return (
     <View style={styles.container}>
-      <Text>fe</Text>
       <FlatList
+        keyExtractor={(item) => item.id} // this will become the key
         data={people}
         renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
       />
-      <Text>fe</Text>
-      <Text>fe</Text>
-      <Text>fe</Text>
-      <Text>fe</Text>
     </View>
   );
 }
